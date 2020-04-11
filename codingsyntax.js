f22e8566213dcc40beafac075f30b264c38c2f28
Donghyun <markk628@gmail.com>
2020-04-11 15:05:10 -0700

    first commit

---
+++ codingsyntax.js
@@ -0,0 +1,15 @@
+function fizzBuzz(targetnum) {
+    for (var i =1; i<targetnum; i++;) {
+        let result = "";
+        if i%3 == 0 {
+            result += "Fizz"
+        } else if i%5 ==0 {
+            result += "Buzz"
+        } else if i%3 > 0 & i%5 > 0 {
+            result = i
+        }
+        console.log(result += "\n")
+    }
+}
+
+fizzBuzz(50)
---
+++ stackClass.py
@@ -0,0 +1,23 @@
+import Linked_List from LinkedList
+
+class Stack():
+    def __init__():
+        self.list = Linked_List()
+
+    def push(newItem):
+        selflist.prepent(newItem, idex = 0)
+
+    def peek():
+        if self.list.head != None:
+            return self.list.head.data[0]
+        else:
+            return None
+
+    def pop():
+        topItem = self.peek()
+        self.list.delete(topItem)
+        return topItem
+
+
+
+
